d96a61c1ad28d1dceef55bfec4a57841c8f2586a
dkroeske <dkroeske@gmail.com>
2023-09-20 16:34:36 +0200

    Added pushbutton

--- pico/hello_world/main.c
+++ pico/hello_world/main.c
@@ -6,9 +6,10 @@
 #include "stdlib.h"
 #include "string.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
 #define LED 1
-#define SW  22
+#define SW  20
 
 int main() {
 
@@ -18,20 +19,31 @@ int main() {
     // Init all I/O
     gpio_init(SW);
     gpio_set_dir(SW, GPIO_IN);
+    gpio_pull_up(SW);
+    
     gpio_init(LED);
     gpio_set_dir(LED, GPIO_OUT);
 
+    int sleep = 0;
     while (true) {
         
+        if(gpio_get(SW)) {
+            sleep = 1000;
+            printf("+\n");
+        } else {
+            sleep = 100;
+            printf("-\n");
+        }       
+
         //LED on and wait
-        sleep_ms(250);
+        sleep_ms(sleep);
         gpio_put(LED, 0);
         
         //LED off and wait
-        sleep_ms(250);
+        sleep_ms(sleep);
         gpio_put(LED, 1);
         
         //Just to inform user
-        printf("+\n");
+//        printf("+\n");
     }
 }
