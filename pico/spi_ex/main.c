6eea16af402aeddeb281057c89590efdec1ca2f1
dkroeske <dkroeske@gmail.com>
2023-09-29 17:56:21 +0200

    spi_ex working now. Added spi protocol images

--- pico/spi_ex/main.c
+++ pico/spi_ex/main.c
@@ -1,6 +1,29 @@
-//
-// Hardware SPI interface to MCP2515 CAN interface
-//
+/* *************************************************
+
+Hardware test SPI interface to MCP2515 CAN interface
+ 
+Just to test the PICO to MCP2515 interface. Connections:
+ 
+MCP2515         PICO            (pin)  
+---------------------------------------------------------
+Vcc    <-->     Vbus            (#40)
+GND    <-->     GND             (#38 or other ground pin)
+CS     <-->     SPI0_CSn        (#22)
+SO     <-->     SPIO_RX         (#21)
+SI     <-->     SPIO_TX         (#25)
+SCK    <-->     SPIO_SCK        (#24)
+INT             Not connected
+
+At startup the mcp2515 chip is reset by SPI where after
+CANSTAT is read. After startup the mcp2515 is in CONFIG
+mode.
+
+(c) dkroeske@gmail.com
+
+v1.0    Initial code
+
+***************************************************/
+
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -16,27 +39,52 @@ void mcp2515_reset();
 void mcp2515_init();
 uint8_t mcp2515_read_register(int8_t addr);
 
-
 // Main loop
 int main() {
 
+    //
     stdio_init_all();
+        
+    // reset mcp2515 -> config mode, see datasheet
+    mcp2515_init();
+    sleep_ms(100);
 
     while (true) {
+
+        // read mcp2515 CANSTAT (0x0E)
+        uint8_t data =  mcp2515_read_register(0x0E);
+        switch (data >> 5) {
+            case 0x00: 
+                printf("Normal Operating mode\n");
+                break;
+            case 0x01: 
+                printf("Sleep mode\n");
+                break;
+            case 0x02: 
+                printf("Loopback mode\n");
+                break;
+            case 0x03: 
+                printf("Listen-Only mode\n");
+                break;
+            case 0x04: 
+                printf("Configuration mode\n");
+                break;
+            default: 
+                printf("Invalid mode!\n");
+                break;
+        } 
         
         // Do something useless
         sleep_ms(2500);
         
-        //Just to inform user
-        printf("+\n");
     }
 }
 
 /* ****************************************************** */
-void mcp_2515_init(void)
+void mcp2515_init(void)
 /*
  * 
- * notes   : 
+ * notes   : Config the spi0 interface
  * version : DMK 2309, initial code
  ******************************************************** */
 {
