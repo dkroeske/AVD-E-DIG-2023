71be621e9796ba77e08b5bb10e04215d75a16611
dkroeske <dkroeske@gmail.com>
2025-06-12 22:26:29 +0200

    Work in progress

--- pico/cmsis-dsp-ex/process.c
+++ pico/cmsis-dsp-ex/process.c
@@ -17,6 +17,8 @@ arm_rfft_fast_instance_f32 rfft;
 float32_t   maxValue;
 uint32_t    maxIndex;
 
+uint32_t    dump = 0;
+
 
 uint16_t buf_size = 0;
 
@@ -37,11 +39,11 @@ void samples_to_float(const uint16_t *src, float *dest, uint16_t size, float sca
 }
 
 // used for fir
-float fir_in[BLOCK_SIZE];
-float fir_out[BLOCK_SIZE];
+float signal[BLOCK_SIZE];
+float signal_bpf[BLOCK_SIZE];
 
 // Handle to the fir instances
-fir_handle_t bpf, q_lpf, i_lpf;
+fir_handle_t bpf, lpf_a, lpf_b;
 
 
 float cos_t[BLOCK_SIZE];
@@ -49,10 +51,35 @@ float msin_t[BLOCK_SIZE];
 
 float q[BLOCK_SIZE];
 float i[BLOCK_SIZE];
-float z[BLOCK_SIZE * 2];
-float z_conj[BLOCK_SIZE * 2];
-float y[BLOCK_SIZE*2];
+float q_lpf[BLOCK_SIZE];
+float i_lpf[BLOCK_SIZE];
+uint8_t bits[BLOCK_SIZE];
 
+struct comp_t {
+    float re, im;
+};
+
+typedef struct comp_t comp_t;
+
+comp_t z[BLOCK_SIZE];
+comp_t z_conj[BLOCK_SIZE];
+comp_t z_conj_p;
+comp_t y;
+
+void comp(comp_t *dest, float re, float im) {
+    dest->re = re;
+    dest->im = im;
+}
+
+void comp_conj(comp_t *dest, const comp_t src) {
+    dest->re = src.re;
+    dest->im = src.im * -1.0;
+}
+
+void comp_mult(comp_t* dest, const comp_t a, const comp_t b) {
+    dest->re = (a.re * b.re) - (a.im * b.im);
+    dest->im = (a.re * b.im) + (a.im * b.re);
+}
 
 void process_init(){
     // Precalc cos en -sin tables to convert to IQ
@@ -62,35 +89,67 @@ void process_init(){
     }
     
     bpf = fir_init(bpf_coeffs, BPF_NUMTAPS, BLOCK_SIZE, 1.0);
-    q_lpf = fir_init(lpf_coeffs, LPF_NUMTAPS, BLOCK_SIZE, 1.0);
-    i_lpf = fir_init(lpf_coeffs, LPF_NUMTAPS, BLOCK_SIZE, 1.0);
+    lpf_a = fir_init(lpf_coeffs, LPF_NUMTAPS, BLOCK_SIZE, 1.0);
+    lpf_b = fir_init(lpf_coeffs, LPF_NUMTAPS, BLOCK_SIZE, 1.0);
 }
 
 // Entry point of processing. 
 void process(const uint16_t *inp, uint16_t *outp, uint16_t size) {
 
     // Convert samples to float
-    samples_to_float(inp, fir_in, BLOCK_SIZE, 255.0f);
+    samples_to_float(inp, signal, BLOCK_SIZE, 255.0f);
 
     // Remove DC -> Highpass fc = 400Hz
-    fir_update(bpf, fir_in, fir_out, BLOCK_SIZE);
+    fir_update(bpf, signal, signal_bpf, BLOCK_SIZE);
 
     // fsk demodulate:
 
+
     // Calculate IQ: (multiply with cos() and -sin()) and lowpass 200 Hz
     for(uint16_t idx = 0; idx < BLOCK_SIZE; idx++) {
-        q[idx] = fir_out[idx] * cos_t[idx];
-        i[idx] = fir_out[idx] * msin_t[idx];
+        q[idx] = signal_bpf[idx] * cos_t[idx];
+        i[idx] = signal_bpf[idx] * msin_t[idx];
     }
-    fir_update(q_lpf, q, fir_out, BLOCK_SIZE);
-    fir_update(i_lpf, i, fir_out, BLOCK_SIZE);
+    fir_update(lpf_a, q, q_lpf, BLOCK_SIZE);
+    fir_update(lpf_b, i, i_lpf, BLOCK_SIZE);
+
 
     // Convert to complex z=a+bj and conj(z)
     for(uint16_t n = 0; n < BLOCK_SIZE; n++) {
-        z[(2*n)] = q[(n)];
-        z[(2*n)+1] = i[n];
-        z_conj[(2*n)] = q[n];
-        z_conj[(2*n)+1] = -1.0 * i[n];
+        comp(&z[n], q_lpf[n], i_lpf[n]);
+        comp_conj(&z_conj[n], z[n]);    
+        if(n == 0) {
+            comp_mult(&y, z[n], z_conj_p);
+            z_conj_p = z[BLOCK_SIZE-1];
+        } else {
+            comp_mult(&y, z[n], z_conj[n-1]);
+        }
+        if( atan2(y.im, y.re) >= 0.0) {
+            bits[n] = 1;
+        } else {
+            bits[n] = 0;
+        }
+
+    }
+
+    
+    if( dump++ == 20 ) {
+    
+        for(uint16_t n = 0; n < BLOCK_SIZE; n++) {
+            printf("%d;%f;%f;%f;%f;%f;%f;%d\n",
+                inp[n],
+                signal[n],
+                signal_bpf[n],
+                q[n],
+                i[n],
+                q_lpf[n],
+                i_lpf[n],
+                bits[n]
+            );
+        }
+
+                
+    
     }
     
         
@@ -103,7 +162,7 @@ void process(const uint16_t *inp, uint16_t *outp, uint16_t size) {
     // Convert back to uint16_t for pwm
     for(uint16_t idx = 0; idx < size; idx++) {
         for(uint16_t idy = 0; idy < 32; idy++) {
-            outp[32*idx+idy] = (uint16_t) (127.0f + fir_out[idx] * 255.0f);
+            outp[32*idx+idy] = (uint16_t) (127.0f + signal[idx] * 255.0f);
         }
     }
 
